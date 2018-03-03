9c00254f8375077c826e1b17f22aae90542f75a0
Max <lfmax223@gmail.com>
2018-03-02 20:12:38 -0600

    Add external config module

---
+++ external-config/src/main/java/com/max/externalconfig/PageController.java
@@ -0,0 +1,17 @@
+package com.max.externalconfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PageController {
+
+    @Value("${pageController.msg}")
+    private String pageControllerMsg;
+
+    @RequestMapping("/")
+    public String home() {
+        return pageControllerMsg;
+    }
+}
