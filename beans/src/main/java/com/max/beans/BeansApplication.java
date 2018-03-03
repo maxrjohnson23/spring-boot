ce21b90b4f641d882bfec3771b1f252380b2d140
Max <lfmax223@gmail.com>
2018-03-02 19:35:39 -0600

    Add beans and configurations for web controllers

--- beans/src/main/java/com/max/beans/BeansApplication.java
+++ beans/src/main/java/com/max/beans/BeansApplication.java
@@ -1,23 +1,30 @@
 package com.max.beans;
 
+import com.max.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
-import java.util.Arrays;
-
+@ComponentScan({"com.max"})
 @SpringBootApplication
 public class BeansApplication {
+
+    @Bean
+    public User user(){
+        return new User("Max", "Johnson");
+    }
 
 	public static void main(String[] args) {
 
 		ApplicationContext ctx = SpringApplication.run(BeansApplication.class, args);
-
 		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-        for (String name : beanNames) {
-            System.out.println(name);
+		for(String bean : beanNames) {
+            System.out.println(bean);
         }
+
+        System.out.println(ctx.getBean("user").toString());
 
 	}
 }
---
+++ beans/src/main/java/com/max/service/NotificationService.java
@@ -0,0 +1,20 @@
+package com.max.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("userNotificationService")
+public class NotificationService {
+
+    public void send() {
+        // do something
+    }
+
+    public void sendAsync() {
+        // do something
+    }
+
+    @Override
+    public String toString() {
+        return "NotificationService{}";
+    }
+}
