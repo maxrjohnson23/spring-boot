54cb0b8988aee0fcdce185f175cfe44ac49849a7
Max <lfmax223@gmail.com>
2018-03-02 19:50:21 -0600

    Add a controller and autowire dependencies

--- beans/src/main/java/com/max/controller/PageController.java
+++ beans/src/main/java/com/max/controller/PageController.java
@@ -1,13 +1,22 @@
 package com.max.controller;
 
+import com.max.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PageController {
 
+    private NotificationService notificationService;
+
+    @Autowired
+    public void setNotificationService(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
     @RequestMapping("/")
     public String home() {
-        return "PageController..";
+        return notificationService.toString();
     }
 }
