e7417e9e47ae1fb23d8f575986dbf65a7f6bb857
Max <lfmax223@gmail.com>
2018-03-02 20:56:09 -0600

    Add datasource bean configuration

---
+++ profiles/src/main/java/com/max/profiles/DataSourceConfig.java
@@ -0,0 +1,22 @@
+package com.max.profiles;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "datasource")
+    @Profile("development")
+    DataSource development() {
+        return new DataSource("my-dev-url", 9999);
+    }
+
+    @Bean(name = "datasource")
+    @Profile("production")
+    DataSource production() {
+        return new DataSource("my-prod-url", 6306);
+    }
+
+}
--- profiles/src/main/java/com/max/profiles/ProfilesApplication.java
+++ profiles/src/main/java/com/max/profiles/ProfilesApplication.java
@@ -2,11 +2,15 @@ package com.max.profiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ProfilesApplication {
 
+
     public static void main(String[] args) {
-        SpringApplication.run(ProfilesApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(ProfilesApplication.class, args);
+        DataSource dataSource = (DataSource) ctx.getBean("datasource");
+        System.out.println(dataSource.toString());
     }
 }
