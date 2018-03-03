cd920ffd5326191718cfdffd40d8682a38ae61c6
Max <lfmax223@gmail.com>
2018-03-02 20:42:51 -0600

    Configuration properties application

--- configuration-properties/src/main/java/com/max/configurationproperties/ConfigurationPropertiesApplication.java
+++ configuration-properties/src/main/java/com/max/configurationproperties/ConfigurationPropertiesApplication.java
@@ -2,11 +2,17 @@ package com.max.configurationproperties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class ConfigurationPropertiesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigurationPropertiesApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(ConfigurationPropertiesApplication.class, args);
+
+        MyAppConfig config = (MyAppConfig) ctx.getBean("myAppConfig");
+        System.out.println(config.toString());
     }
 }
